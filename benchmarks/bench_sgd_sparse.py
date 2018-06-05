e9a98d7a3730c92e6e0ddfe7ffe99d43b6b35b9a
Jenna Liu <pumpkin880201@gmail.com>
2018-06-04 22:45:03 -0400

    Add benchmark for SGDRegressor with csr_matrix

---
+++ benchmarks/bench_sgd_sparse.py
@@ -0,0 +1,52 @@
+import numpy as np
+from scipy.sparse import random
+from scipy.sparse.csr import csr_matrix
+from sklearn.linear_model.stochastic_gradient import SGDRegressor
+from sklearn.linear_model import Ridge
+from sklearn.metrics import r2_score
+
+np.random.seed(42)
+
+
+n_samples, n_features = 5000, 300
+density = 0.1
+X = random(n_samples, n_features, density=density).A
+print("input data sparsity: %f" % density)
+coef = 3 * np.random.randn(n_features)
+intercept = 3 * np.random.randn(n_samples)
+y = np.dot(X, coef) + intercept
+
+# add noise
+y += 0.01 * np.random.normal((n_samples,))
+
+# Split data in train set and test set
+n_samples = X.shape[0]
+X_train, y_train = X[:n_samples // 2], y[:n_samples // 2]
+X_test, y_test = X[n_samples // 2:], y[n_samples // 2:]
+
+
+###############################################################################
+def benchmark(clf, X_train, X_test, case):
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_train)
+    r2 = r2_score(y_train, y_pred)
+    print("r^2 on train data (%s) : %f" % (case, r2))
+
+    y_pred = clf.predict(X_test)
+    r2 = r2_score(y_test, y_pred)
+    print("r^2 on test data (%s) : %f" % (case, r2))
+
+
+clf = SGDRegressor(max_iter=2000, tol=None)
+benchmark(clf, X_train, X_test, "SGDRegressor numpy array")
+
+clf = SGDRegressor(max_iter=2000, tol=None)
+benchmark(clf, csr_matrix(X_train), csr_matrix(X_test),
+          "SGDRegressor csr_matrix")
+
+clf = Ridge()
+benchmark(clf, X_train, X_test, "Ridge numpy array")
+
+clf = Ridge()
+benchmark(clf, csr_matrix(X_train), csr_matrix(X_test), "Ridge csr_matrix")
